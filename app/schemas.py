e8e737d712459c375f773bf5599f227086230cad
Nayan <nayan92001@gmail.com>
2023-03-20 20:24:34 +0530

    uncomment emotion code

--- app/schemas.py
+++ app/schemas.py
@@ -20,40 +20,46 @@ class VideoTransformTrack(MediaStreamTrack):
         super().__init__()
         self.track = track
         self.transform = transform
-        self.emotion_dict = {0: "0: Angry", 1: "1: Disgusted", 2: "2: Fearful", 3: "3: Happy", 4: "4: Neutral", 5: "5: Sad", 6: "6: Surprised"}
+        self.emotion_dict = {0: "0: Angry", 1: "1: Disgusted", 2: "2: Fearful",
+                             3: "3: Happy", 4: "4: Neutral", 5: "5: Sad", 6: "6: Surprised"}
         self.face_detector = face_detector
         self.emotion_model = emotion_model
+
+    
+
 
     async def recv(self):
         frame = await self.track.recv()
 
-        if self.transform =='test':
+        if self.transform == 'test':
             img = frame.to_ndarray(format='bgr24')
-            cv.rectangle(img, (100,100), (200,200), (0,0,255), 4)
+            cv.rectangle(img, (100, 100), (200, 200), (0, 0, 255), 4)
             nframe = VideoFrame.from_ndarray(img, format='bgr24')
             nframe.pts = frame.pts
             nframe.time_base = frame.time_base
             return nframe
 
         elif self.transform == 'emotion':
+            
             img = frame.to_ndarray(format='bgr24')
             gray = frame.to_ndarray(format='gray')
 
-            faces = self.face_detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-            for (x,y,w,h) in faces:
+            faces = self.face_detector.detectMultiScale(
+                gray, scaleFactor=1.3, minNeighbors=5)
+            for (x, y, w, h) in faces:
                 cv.rectangle(img, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
                 roi_gray_frame = gray[y:y + h, x:x + w]
                 cropped_img = np.expand_dims(np.expand_dims(cv.resize(roi_gray_frame, (48, 48)), -1), 0)
 
-                # emotion_prediction = self.emotion_model.predict(cropped_img)
-                # maxindex = int(np.argmax(emotion_prediction))
-                # maxval =  "{:.2f}".format(np.amax(emotion_prediction)*100)
+                emotion_prediction = self.emotion_model.predict(cropped_img)
+                maxindex = int(np.argmax(emotion_prediction))
+                maxval =  "{:.2f}".format(np.amax(emotion_prediction)*100)
 
-                # print(maxindex)
-                # print(maxval)
+                print(maxindex)
+                print(maxval)
 
-                # cv.putText(img, self.emotion_dict[maxindex], (x+5, y-20), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv.LINE_AA)
-                # cv.putText(img, str(maxval)+"%", (x+200, y-20), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv.LINE_AA)
+                cv.putText(img, self.emotion_dict[maxindex], (x+5, y-20), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv.LINE_AA)
+                cv.putText(img, str(maxval)+"%", (x+200, y-20), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv.LINE_AA)
 
             nframe = VideoFrame.from_ndarray(img, format="bgr24")
             nframe.pts = frame.pts
